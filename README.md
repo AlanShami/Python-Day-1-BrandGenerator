f8e61bb9941713ed5eeeb3555b24217f1d76fda4
Alan Shami <alanshami@Alans-Air.attlocal.net>
2024-01-19 18:25:55 -0800

    First Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+##Day-1_of_Python-BrandGenerator
+
+Credit: [Angela Yu] https://www.udemy.com/share/101Xwi3@Cd5H-edT3Ktu-_5Z3i14xX7vfWAqQSqiDCRRkMUqlu0NXefLQnWpT9ksckT6u1rs-g==/
+
+This is day 1 of 100 of learning python and creating a project in python
+
+#10/02/202
+
+# Basics Python (print function, input function, Strings, debugging, variables)
+# ---------------------------------------------
+
+# print("Hello " + input("What is your name? "))
+
+# name = input("What is your name?\n")
+
+# print(type(name))
+
+- [main.py]()
---
+++ main.py
@@ -0,0 +1,21 @@
+# Day One -- Band Generator
+# 10/02/202
+
+# Basics Python (print function, input function, Strings, debugging, variables)
+# ---------------------------------------------
+
+# print("Hello " + input("What is your name? "))
+
+# name = input("What is your name?\n")
+
+# print(type(name))
+
+# Project Time -- Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the city you grew up in?\n")
+
+petName = input("What's your pet's name?\n")
+
+print("Your band name could be " + city + " " + petName)
